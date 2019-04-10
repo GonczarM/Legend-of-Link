0405ce6f5d5b1790d6d57ed424b818705ae64f8b
Matthew Gonczar <mattisagumball@gmail.com>
2019-04-10 00:37:12 -0500

    new images and more fluid game

--- js/app.js
+++ js/app.js
@@ -1,3 +1,8 @@
+$(document).on('click', (event) => {
+	$('#start').hide("fade", "slow");
+	$('#zelda').css('display', 'flex');
+});
+
 canvas = document.getElementById('zelda');
 ctx = canvas.getContext('2d');
 
@@ -12,22 +17,22 @@ const row = 16;
 const map = [
 	1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,
 	1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,
-	1,0,1,0,0,0,1,1,1,0,1,1,1,1,0,1,
-	1,0,1,0,0,0,1,0,0,0,1,0,0,0,0,1,
-	1,0,1,0,0,0,1,1,0,0,1,0,1,1,0,1,
-	1,0,1,0,0,0,1,0,0,0,1,0,0,1,0,1,
-	1,0,1,1,1,0,1,1,1,0,1,1,1,1,0,1,
+	1,0,2,0,0,0,2,2,2,0,2,2,2,2,0,1,
+	1,0,2,0,0,0,2,0,0,0,2,0,0,0,0,1,
+	1,0,2,0,0,0,2,2,0,0,2,0,2,2,0,1,
+	1,0,2,0,0,0,2,0,0,0,2,0,0,2,0,1,
+	1,0,2,2,2,0,2,2,2,0,2,2,2,2,0,1,
 	1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,
-	1,0,0,0,1,1,1,0,0,1,1,1,0,0,0,1,
-	1,0,0,0,1,0,1,0,0,1,0,0,0,0,0,1,
-	1,0,0,0,1,0,1,0,0,1,1,0,0,0,0,1,
-	1,0,0,0,1,1,1,0,0,1,0,0,0,0,0,1,
+	1,0,0,0,2,2,2,0,0,2,2,2,0,0,0,1,
+	1,0,0,0,2,0,2,0,0,2,0,0,0,0,0,1,
+	1,0,0,0,2,0,2,0,0,2,2,0,0,0,0,1,
+	1,0,0,0,2,2,2,0,0,2,0,0,0,0,0,1,
 	1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,
-	1,0,1,1,1,0,1,1,1,1,0,1,0,0,0,1,
-	1,0,0,0,1,0,1,0,0,0,0,1,0,0,0,1,
-	1,0,0,1,0,0,1,1,1,0,0,1,0,0,0,1,
-	1,0,1,0,0,0,1,0,0,0,0,1,0,0,0,1,
-	1,0,1,1,1,0,1,1,1,1,0,1,1,1,0,1,
+	1,0,2,2,2,0,2,2,2,2,0,2,0,0,0,1,
+	1,0,0,0,2,0,2,0,0,0,0,2,0,0,0,1,
+	1,0,0,2,0,0,2,2,2,0,0,2,0,0,0,1,
+	1,0,2,0,0,0,2,0,0,0,0,2,0,0,0,1,
+	1,0,2,2,2,0,2,2,2,2,0,2,2,2,0,1,
 	1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,
 	1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,
 ];
@@ -45,7 +50,7 @@ class Sprite {
 class Enemy {
 	constructor(x, y, picture){
 		this.alive = true;
-		this.speed = 1;
+		this.speed = 2;
 		this.image = new Image();
 		this.image.src = picture;
 		this.x = x;
@@ -75,20 +80,28 @@ class Enemy {
 	die () {
 		this.alive = false;
 		this.x = 15*tileW
-		this.y = 0 
+		this.y = 0
 	}
 }
 
 const grass = new Sprite("images/grass.png");
-const link = new Sprite("images/link.png");
 const dirt = new Sprite("images/dirt.png");
-const sword = new Sprite("images/sword.png");
+const swordUp = new Sprite("images/sword up.png");
+const swordDown = new Sprite("images/sword down.png");
+const swordLeft = new Sprite("images/sword left.png");
+const swordRight = new Sprite("images/sword right.png");
+const linkForward = new Sprite("images/link forward.png");
+const linkBackward = new Sprite("images/link backward.png");
+const linkLeft = new Sprite("images/link left.png");
+const linkRight = new Sprite("images/link right.png");
+const wall = new Sprite("images/wall.png");
 
 const player = {
 	x: tileW*8,
 	y: tileH*10,
 	unsheath: false,
-	speed: 2, 
+	speed: 2,
+	attackTime: 1,
 	// tileY: null,
 	// tileX: null,
 	direction: {
@@ -98,12 +111,21 @@ const player = {
     right: false		
 	},
 	draw(){
-		link.draw(this.x, this.y)
+		linkForward.draw(this.x, this.y)
 	},
 	move(){
-		if(this.direction.up) this.y -= this.speed;
-    if(this.direction.left) this.x -= this.speed;
-    if(this.direction.right) this.x += this.speed;
+		if(this.direction.up){
+			linkBackward.draw(this.x, this.y) 
+			this.y -= this.speed;
+		}
+    if(this.direction.left){
+    	linkLeft.draw(this.x, this.y)
+   	  this.x -= this.speed;
+   	}
+    if(this.direction.right){
+    	linkRight.draw(this.x, this.y)
+    	this.x += this.speed;
+    }
     if(this.direction.down) this.y += this.speed;  
 	},
 	startDirection(key){
@@ -120,57 +142,65 @@ const player = {
   },
   attack(enemy){
   	for(let i = 0; i < 6; i++){
-	  	if(this.direction.up == true && this.unsheath == true){
-	  		sword.draw(this.x, this.y - tileH)
-	  		if(
-	  			this.y - tileH < enemy.y + tileH &&
-	  			this.y + tileH > enemy.y &&
-	  			this.x < enemy.x + tileW &&
-	  			this.x + tileW > enemy.x
-	  			){
-	 				ghosts[i].die();
-	  		}
-	  	}
-	  	if(this.direction.left == true && this.unsheath == true){
-	  		sword.draw(this.x - tileW, this.y)
-	  		if(
-	  			this.x - tileW < enemy.x + tileW &&
-	  			this.y + tileH > enemy.y &&
-	  			this.x > enemy.x &&
-	  			this.y < enemy.y + tileH
-	  			){
-	  			ghosts[i].die();
-	  		}
-	  	}
-	  	if(this.direction.right == true && this.unsheath == true){
-	  		sword.draw(this.x + tileW, this.y)
-	  		if(
-	  			this.x + (2*tileW) > enemy.x &&
-	  			this.x + tileW < enemy.x + tileW &&
-	  			this.y + tileH > enemy.y &&
-	  			this.y < enemy.y + tileH
-	  			){
-	  			ghosts[i].die();
-	  		}
-	  	}
-	  	if(this.direction.down == true && this.unsheath == true){
-	  		sword.draw(this.x, this.y + tileH)
-	  		if(
-	  			this.y + (2*tileH) > enemy.y &&
-	  			this.x < enemy.x + tileW &&
-	  			this.x + tileW > enemy.x &&
-	  			this.y + tileH < enemy.y + tileH
-	  			){
-	  			ghosts[i].die();
-	  			console.log(ghosts[i]);
-	  		}
-	  	}  
-	  } 	
+		 	if(this.direction.up == true && this.unsheath == true){
+		  	swordUp.draw(this.x, this.y - tileH)
+		  	if(
+		  		this.y - tileH < enemy.y + tileH &&
+		  		this.y + tileH > enemy.y &&
+		  		this.x < enemy.x + tileW &&
+		  		this.x + tileW > enemy.x
+		  		){
+		 			ghosts[i].die();
+		  	}
+		  }
+		  if(this.direction.left == true && this.unsheath == true){
+		  	swordLeft.draw(this.x - tileW, this.y)
+		  	if(
+		  		this.x - tileW < enemy.x + tileW &&
+		  		this.y + tileH > enemy.y &&
+		  		this.x > enemy.x &&
+		  		this.y < enemy.y + tileH
+		  		){
+		  		ghosts[i].die();
+		  		// return;
+		  	}
+		  }
+		  if(this.direction.right == true && this.unsheath == true){
+		  	swordRight.draw(this.x + tileW, this.y)
+		  	if(
+		  		this.x + (2*tileW) > enemy.x &&
+		  		this.x + tileW < enemy.x + tileW &&
+		  		this.y + tileH > enemy.y &&
+		  		this.y < enemy.y + tileH
+		  		){
+		  		ghosts[i].die();
+		  		// return;
+		  	}
+		  }
+		  if(this.direction.down == true && this.unsheath == true){
+		  	swordDown.draw(this.x, this.y + tileH)
+		  	if(
+		  		this.y + (2*tileH) > enemy.y &&
+		  		this.x < enemy.x + tileW &&
+		  		this.x + tileW > enemy.x &&
+		  		this.y + tileH < enemy.y + tileH
+		  		){
+		  		ghosts[i].die();
+		  		// return;
+		  	}
+		  }
+		}  	   	
   },
-  startAttack(key,){
+  startAttack(key){
   	if(key == ' '){
   		this.unsheath = true;
-  	} 	
+  	}
+  	this.timerHandle = setInterval(() => {
+  		this.attackTime--
+  		if(this.attackTime <= 0){
+  			this.stopAttack();
+  		}
+  	}, 1000)
   },
   stopAttack(key){
   	this.unsheath = false;
@@ -216,7 +246,15 @@ const player = {
   	else{
   		return false;
   	}
-  }
+  },
+  // slayedThemAll(){
+  // 	if(ghosts.every(ghosts.alive == false)){
+  // 		return true;
+  // 	}
+  // 	else{
+  // 		return false;
+  // 	}
+  // }
 }
 
 const spawnPoints = [
@@ -225,12 +263,18 @@ const spawnPoints = [
 	[4*tileW, 15*tileH],
 	[11*tileW, 4*tileH],
 	[11*tileW, 10*tileH],
-	[11*tileW, 10*tileH]
+	[11*tileW, 15*tileH]
 ];
 
 const ghosts = [];
+const ghost1 = [];
+const ghost2 = [];
+const ghost3 = [];
+const ghost4 = [];
+const ghost5 = [];
+const ghost6 = [];
 
-const createEnemies = function(){
+function createEnemies(){
  	for(let i = 0; i < 6; i++){
  		ghosts[i] = new Enemy(spawnPoints[i][0],
  		spawnPoints[i][1], "images/enemies.png")
@@ -246,40 +290,43 @@ function drawGame(){
 			let tileY = y*tileH;
 			let tileType = map[mapIndex];
 			if(tileType === 1){
-				grass.draw(tileX, tileY);
+				wall.draw(tileX, tileY);
 			}
 			if(tileType === 0){
 				dirt.draw(tileX, tileY);
+			}
+			if(tileType === 2){
+				grass.draw(tileX, tileY);
 			}
 		}
 	}
 };
 
 function gameOver() {
-  document.write(`
-    <h1>YOU ARE DEAD YOU SHOULD NOT HAVE CRASHED INTO THAT</h1>
-    <FORM>
-      <INPUT TYPE="hidden" VALUE="you also shouldn't capitalize your html or use STYLE='' because it's not 1995">
-      <BUTTON STYLE="font-size: 18pt">CLICK</BUTTON>
-    </FORM>
-  `)  
+	$('#zelda').css('display', 'none')
+  $('.lose').show('fade', 1000)
 }
+
+function youWin(){
+	$('#zelda').css('display', 'none')
+	$('.win').show('fade', 2000)	
+} 
 
 function animate() {
 	for(let i = 0; i < 6; i++){
 		ghosts[i].move();
 	}
-	player.move();
 	clearCanvas();
 	drawGame();
+	player.draw();
+	player.move();
+	for(let i = 0; i < 6; i++){
+		player.attack(ghosts[i]);
+	}
 	for(let i = 0; i < 6; i++){
 		if (ghosts[i].alive) {
 			ghosts[i].draw();
 		}
-	}
-	player.draw();
-	for(let i = 0; i < 6; i++){
-		player.attack(ghosts[i]);
 	}
 	for(let i = 0; i < 6; i++){
 		ghosts[i].checkCollisionWall();
@@ -291,6 +338,11 @@ function animate() {
 			return;
 		}
 	}
+
+	// if(player.slayedThemAll()){
+	// 	youWin();
+	// 	return;
+	// }
 	window.requestAnimationFrame(animate);
 }
 animate();
