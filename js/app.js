cff000014ac367ea788412eaa99615ff1dd0e5c3
Matthew Gonczar <mattisagumball@gmail.com>
2019-07-26 15:47:06 -0500

    last try

--- js/app.js
+++ js/app.js
@@ -506,22 +506,3 @@ document.addEventListener('keydown', (e) => {
     e.preventDefault();
   }
 });
-
-(function($) {
-  const IS_IOS = /iphone|ipad/i.test(navigator.userAgent);
-  $.fn.nodoubletapzoom = function() {
-    if (IS_IOS)
-      $(this).bind('touchstart', function preventZoom(e) {
-        const t2 = e.timeStamp
-          , t1 = $(this).data('lastTouch') || t2
-          , dt = t2 - t1
-          , fingers = e.originalEvent.touches.length;
-        $(this).data('lastTouch', t2);
-        if (!dt || dt > 500 || fingers > 1) return; // not double-tap
-
-        e.preventDefault(); // double tap - prevent the zoom
-        // also synthesize click events we just swallowed up
-        $(this).trigger('click').trigger('click');
-      });
-  };
-})(jQuery);
